9814422bec20e9cb561ec9794d2309daaf625f5d
Win <tranvietthang2994@gmail.com>
2025-10-01 01:05:20 +0700

    version 3.2

--- src/app/api/blog/[slug]/route.ts
+++ src/app/api/blog/[slug]/route.ts
@@ -1,8 +1,8 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { getSupabaseAdmin } from "@/services/supabase-admin";
 
 export async function GET(
-  request: NextRequest,
+  request: Request,
   { params }: { params: { slug: string } }
 ) {
   try {
